8e7c4ebdfa245bc4ff524ed0df815eacdfa529d5
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-18 19:04:44 +0100

    container, header and skills comp. added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Header } from "./common/Header";
 import { Skills } from "./common/Skills";
+import { Container } from "./common/Container";
 
 function App() {
   return (
-    <>
+    <Container>
       <Header />
       <Skills />
-    </>
+    </Container>
   );
 }
 

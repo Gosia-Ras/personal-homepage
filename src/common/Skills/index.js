1946abec783d7cb61166eea9a9e4a2beaa588bc7
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-23 11:00:59 +0100

    another skills box added

--- src/common/Skills/index.js
+++ src/common/Skills/index.js
@@ -1,36 +1,66 @@
-import { SkillsList, SkillsContainer, SkillsBox, ListItem, SkillsHeader } from "./styled";
-import { VscTools } from "react-icons/vsc";
+import {
+  SkillsList,
+  SkillsContainer,
+  SkillsBox,
+  ListItem,
+  SkillsHeader,
+} from "./styled";
+import { VscTools, VscRocket } from "react-icons/vsc";
 
 export const Skills = () => {
   return (
-    <SkillsBox>
-      <SkillsHeader>My skillset includes <VscTools /></SkillsHeader>
-      <SkillsContainer>
-        <SkillsList>
-          <ListItem>html</ListItem>
-          <ListItem>css</ListItem>
-          <ListItem>react</ListItem>
-          <ListItem>javascript</ListItem>
-          <ListItem>Error handling</ListItem>
-          <ListItem>Redux-Saga</ListItem>
-        </SkillsList>
-        <SkillsList>
-          <ListItem>react hooks</ListItem>
-          <ListItem>styled-components</ListItem>
-          <ListItem>redux</ListItem>
-          <ListItem>react router</ListItem>
-          <ListItem>Error handling</ListItem>
-          <ListItem>Redux-Saga</ListItem>
-        </SkillsList>
-        <SkillsList>
-          <ListItem>Teamwork</ListItem>
-          <ListItem>RWD</ListItem>
-          <ListItem>CSS Grid</ListItem>
-          <ListItem>CSS Flexbox</ListItem>
-          <ListItem>Redux-Saga</ListItem>
-          <ListItem>Error handling</ListItem>
-        </SkillsList>
-      </SkillsContainer>
-    </SkillsBox>
+    <>
+      <SkillsBox>
+        <SkillsHeader>
+          My skillset includes <VscTools />
+        </SkillsHeader>
+        <SkillsContainer>
+          <SkillsList>
+            <ListItem>HTML5</ListItem>
+            <ListItem>CSS3</ListItem>
+            <ListItem>CSS Grid</ListItem>
+            <ListItem>CSS Flexbox</ListItem>
+            <ListItem>styled-components</ListItem>
+          </SkillsList>
+          <SkillsList>
+            <ListItem>React</ListItem>
+            <ListItem>JavaScript</ListItem>
+            <ListItem>Error handling</ListItem>
+            <ListItem>React Hooks</ListItem>
+            <ListItem>Redux</ListItem>
+          </SkillsList>
+          <SkillsList>
+            <ListItem>React Router</ListItem>
+            <ListItem>Redux-Saga</ListItem>
+            <ListItem>Teamwork</ListItem>
+            <ListItem>RWD</ListItem>
+          </SkillsList>
+        </SkillsContainer>
+      </SkillsBox>
+      <SkillsBox>
+        <SkillsHeader>
+          What I want to learn next <VscRocket />
+        </SkillsHeader>
+        <SkillsContainer>
+          <SkillsList>
+            <ListItem>Node.js</ListItem>
+            <ListItem>MongoDB</ListItem>
+            <ListItem>TypeScript</ListItem>
+          </SkillsList>
+          <SkillsList>
+            {" "}
+            <ListItem>Bootstrap</ListItem>
+            <ListItem>Tailwind CSS</ListItem>
+            <ListItem>Material UI</ListItem>
+          </SkillsList>
+          <SkillsList>
+            {" "}
+            <ListItem>GatsbyJS</ListItem>
+            <ListItem>Vue.js</ListItem>
+            <ListItem>jQuery</ListItem>
+          </SkillsList>
+        </SkillsContainer>
+      </SkillsBox>
+    </>
   );
 };
--- src/common/Skills/styled.js
+++ src/common/Skills/styled.js
@@ -5,7 +5,7 @@ export const SkillsBox = styled.div`
   border-radius: 4px;
   max-width: 1216px;
   margin: 0 auto;
-  padding: 10px 40px 10px 20px;
+  padding: 10px 50px 0px 15px;
   margin-top: 63px;
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
     0px 16px 58px rgba(9, 10, 51, 0.03);
@@ -15,7 +15,6 @@ export const SkillsContainer = styled.div`
   max-width: 1151px;
   margin: 0 auto;
   display: flex;
-  gap: 50px;
   justify-content: space-between;
 `;
 
@@ -27,6 +26,8 @@ export const SkillsHeader = styled.h1`
   display: flex;
   align-items: center;
   gap: 8px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.greyDetails};
 `;
 
 export const SkillsList = styled.ul`
@@ -37,7 +38,6 @@ export const ListItem = styled.li`
   font-family: "Inter", sans-serif;
   font-size: 18px;
   font-weight: 400;
-  letter-spacing: 5%;
   color: ${({ theme }) => theme.colors.textSecondary};
   line-height: 25px;
   &:before {

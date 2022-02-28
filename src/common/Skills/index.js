9fce9c2eca0d69664a3166c0c68a667c13e2e9e8
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-28 15:28:18 +0100

    list items mapped

--- src/common/Skills/index.js
+++ src/common/Skills/index.js
@@ -6,6 +6,7 @@ import {
   SkillsHeader,
 } from "./styled";
 import { VscTools, VscRocket } from "react-icons/vsc";
+import { SkillsListed, SkillsToDo } from "../../getSkillsList";
 
 export const Skills = () => {
   return (
@@ -15,26 +16,7 @@ export const Skills = () => {
           My skillset includes <VscTools />
         </SkillsHeader>
         <SkillsContainer>
-          <SkillsList>
-            <ListItem>HTML5</ListItem>
-            <ListItem>CSS3</ListItem>
-            <ListItem>CSS Grid</ListItem>
-            <ListItem>CSS Flexbox</ListItem>
-            <ListItem>styled-components</ListItem>
-          </SkillsList>
-          <SkillsList>
-            <ListItem>React</ListItem>
-            <ListItem>JavaScript</ListItem>
-            <ListItem>Error handling</ListItem>
-            <ListItem>React Hooks</ListItem>
-            <ListItem>Redux</ListItem>
-          </SkillsList>
-          <SkillsList>
-            <ListItem>React Router</ListItem>
-            <ListItem>Redux-Saga</ListItem>
-            <ListItem>Teamwork</ListItem>
-            <ListItem>RWD</ListItem>
-          </SkillsList>
+          <SkillsListed />
         </SkillsContainer>
       </SkillsBox>
       <SkillsBox>
@@ -42,23 +24,7 @@ export const Skills = () => {
           What I want to learn next <VscRocket />
         </SkillsHeader>
         <SkillsContainer>
-          <SkillsList>
-            <ListItem>Node.js</ListItem>
-            <ListItem>MongoDB</ListItem>
-            <ListItem>TypeScript</ListItem>
-          </SkillsList>
-          <SkillsList>
-            {" "}
-            <ListItem>Bootstrap</ListItem>
-            <ListItem>Tailwind CSS</ListItem>
-            <ListItem>Material UI</ListItem>
-          </SkillsList>
-          <SkillsList>
-            {" "}
-            <ListItem>GatsbyJS</ListItem>
-            <ListItem>Vue.js</ListItem>
-            <ListItem>jQuery</ListItem>
-          </SkillsList>
+          <SkillsToDo />
         </SkillsContainer>
       </SkillsBox>
     </>
---
+++ src/getSkillsList.js
@@ -0,0 +1,22 @@
+import { SkillsList, ListItem } from "./common/Skills/styled";
+import { goals, items } from "./skillsList";
+
+export const SkillsListed = () => {
+  return (
+    <SkillsList>
+      {items.map((item) => (
+        <ListItem key={item.id}>{item.name}</ListItem>
+      ))}
+    </SkillsList>
+  );
+};
+
+export const SkillsToDo = () => {
+  return (
+    <SkillsList>
+      {goals.map((goal) => (
+        <ListItem key={goal.id}>{goal.name}</ListItem>
+      ))}
+    </SkillsList>
+  );
+};

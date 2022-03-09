cf64e494c6dd88a8e87949227f5a9cf102963580
Gosia_Ras <ras.malgorzata@gmail.com>
2022-03-09 13:58:04 +0100

    normalize added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./store";
+import Normalize from "react-normalize";
 import DarkThemeProvider from "./features/DarkThemeProvider";
 import { Container } from "./common/Container/styled";
 import { Header } from "./common/Header";
@@ -11,6 +12,7 @@ import { Portfolio } from "./common/Portfolio";
 export const App = () => (
   <ReduxProvider store={store}>
     <DarkThemeProvider>
+      <Normalize />
       <Container>
         <Header />
         <Skills />

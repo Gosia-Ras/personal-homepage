2916e992f563e7a89fa0e68d3f34846ac4d1937d
Gosia_Ras <ras.malgorzata@gmail.com>
2022-03-14 11:52:36 +0100

    theme preference saves to local storage

--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-let initialState = {};
+const localStorageKey = "theme";
+const localTheme = localStorage.getItem(localStorageKey);
+
+let initialState = {
+  preferences: localTheme ? JSON.parse(localTheme) : {},
+};
 
 const store = createStore(rootReducer, initialState);
+
+store.subscribe(() => {
+  const preferences = store.getState().preferences;
+  if (!preferences) return;
+
+  localStorage.setItem(localStorageKey, JSON.stringify(preferences));
+});
 export default store;
